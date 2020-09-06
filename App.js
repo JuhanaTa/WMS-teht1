ba8bdf7d89884688c80726d84f7b6918f8776320
Niklas Nilsson <48365153+Jalsson@users.noreply.github.com>
2020-09-06 11:43:04 +0300

    added import, second try to push and merge

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {yeet} from './app.json'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! THis is test</Text>
-
       <Text>Niklas was here</Text>
-
       <Text>This is just testing for github task</Text>
-
+      <button>this is some random button</button>
       <StatusBar style="auto" />
     </View>
   );
