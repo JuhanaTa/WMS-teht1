5c08d7d9221b8256a9b8565a30f59533c5a3bf57
Niklas Nilsson <48365153+Jalsson@users.noreply.github.com>
2020-09-06 11:32:21 +0300

    Added new text to master

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! THis is test</Text>
+      <Text>Niklas was here</Text>
       <StatusBar style="auto" />
     </View>
   );
